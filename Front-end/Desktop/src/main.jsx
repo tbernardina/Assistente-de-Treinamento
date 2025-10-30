71b6688d1f19fd25a22e9fda78bbcd8bd9a18c32
Thiago Reis Dalla Bernardina <trdallabernardina@gmail.com>
2025-10-30 16:34:18 -0300

    Update main.jsx

--- Front-end/Desktop/src/main.jsx
+++ Front-end/Desktop/src/main.jsx
@@ -1,11 +1,11 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import ChatBubble from "./components/ChatBubble/ChatBubble.jsx"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ChatBubble from "./components/ChatBubble/ChatBubble.jsx";
+import "./styles/global.css";
 
+console.log("[main.jsx] carregado");
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ChatBubble/>
-  </React.StrictMode>,
+    <ChatBubble />
+  </React.StrictMode>
 );
-
-import './components/ChatBubble/main.js';
